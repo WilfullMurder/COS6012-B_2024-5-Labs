e37d24462c189c45bc134bcdb66e8d4396fbc024
SameerPatwari <sameerpatwari2003@gmail.com>
2025-01-30 02:46:55 +0530

    refactor: replace manual thread creation with ExecutorService thread pool

--- src/main/java/lab1/recipe06/Main.java
+++ src/main/java/lab1/recipe06/Main.java
@@ -2,18 +2,24 @@ package lab1.recipe06;
 
 import java.util.Deque;
 import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Deque<Event> deque = new ConcurrentLinkedDeque<>();
 
         WriterTask writer = new WriterTask(deque);
 
-        for(int i = 0; i  < Runtime.getRuntime().availableProcessors(); i++){
-            Thread thread = new Thread(writer);
-            thread.start();
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() + 1);
+
+        for (int i = 0; i < Runtime.getRuntime().availableProcessors(); i++) {
+            executorService.submit(writer);
         }
+
         CleanerTask cleaner = new CleanerTask(deque);
-        cleaner.start();
+        executorService.submit(cleaner);
+
+        executorService.shutdown();
     }
 }
