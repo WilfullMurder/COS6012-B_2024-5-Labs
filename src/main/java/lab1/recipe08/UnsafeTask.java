8c5643d92bdb557cb2f24639f79a61b05bb5b532
wilfullmurder <mrfarrow2013@gmail.com>
2025-01-30 09:40:36 +0000

    Adds the code and explanation for recipe8

---
+++ src/main/java/lab1/recipe08/UnsafeTask.java
@@ -0,0 +1,23 @@
+package lab1.recipe08;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class UnsafeTask implements Runnable{
+    private Date startDate;
+
+
+    @Override
+    public void run() {
+        startDate = new Date();
+        System.out.printf("Starting Thread: %s : %s\n", Thread.currentThread().getId(), startDate);
+
+        try{
+            TimeUnit.SECONDS.sleep((int)Math.rint(Math.random() * 10));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.printf("Thread Finished: %s : %s\n", Thread.currentThread().getId(),startDate);
+    }
+}
