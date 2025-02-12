32d556f47ba3ad537b678c542003e989bf8b214e
WilfullMurder <81480788+WilfullMurder@users.noreply.github.com>
2025-02-12 19:48:41 +0000

    Adds code and README for lab2-recipe03 (#28)

--- src/main/java/lab2/recipe03/Main.java
+++ src/main/java/lab2/recipe03/Main.java
@@ -2,6 +2,28 @@ package lab2.recipe03;
 
 public class Main {
     public static void main(String[] args){
+        System.out.print("Running example with fair-mode = false\n");
+        testPrintQueue(false);
+        System.out.print("Running example with fair-mode = true\n");
+        testPrintQueue(true);
+    }
 
+    private static void testPrintQueue(boolean fairMode){
+        PrintQueue printQueue = new PrintQueue(fairMode);
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new Job(printQueue), "Thread " + i);
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
